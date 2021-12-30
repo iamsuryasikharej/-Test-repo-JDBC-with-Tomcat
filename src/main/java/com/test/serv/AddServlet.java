bd9321be603706d604a352ae7d1214419095df2a
surya sikha rej <iamssrofficial@gmail.com>
2021-12-30 14:32:59 +0530

    every thing working(css missing)

--- src/main/java/com/test/serv/AddServlet.java
+++ src/main/java/com/test/serv/AddServlet.java
@@ -2,6 +2,7 @@ package com.test.serv;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.List;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -41,9 +42,38 @@ public class AddServlet extends HttpServlet {
 		student.setName(name);
 		student.setRoll(roll);
 		StudentService ss=new StudentService();
-		ss.addStuent(student);
-//		if(b!=false)
-//		System.out.println("success");
-	}
+		boolean b=ss.addStuent(student);
+		if(b!=false)
+		System.out.println("success");
+		List<Student> listOfAllStudents=ss.getAllStudents();
+		PrintWriter pw=response.getWriter();
+		pw.print("<html>");
+		pw.print("<body>");
+		pw.print("<table align='center' bgcolor='pink'>");
+		pw.print("<tr>");
+		pw.print("<th>"+"name");
+		pw.print("</th>");
+		pw.print("<th>"+"roll");
+		pw.print("</th>");
+		
+		pw.print("</tr>");
+		for(Student s : listOfAllStudents)
+		{
+			pw.print("<tr>");
+			pw.print("<td>"+s.getName()+"</td>");
+			pw.print("<td>"+s.getRoll()+"</td>");
+			
+			pw.print("</tr>");
+		}
+		
+		pw.print("</table>");
+		
+		pw.print("</body>");
+		
+		pw.print("</html>");
+		RequestDispatcher rd=request.getRequestDispatcher("./login.html");
+		rd.include(request, response);
+		
+		}
 
 }
--- src/main/java/com/test/service/StudentService.java
+++ src/main/java/com/test/service/StudentService.java
@@ -4,14 +4,20 @@ package com.test.service;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.mysql.cj.protocol.Resultset;
 import com.test.bean.Student;
 
 public class StudentService {
 	Connection con;
 	Student stud;
 	Boolean b;
-	public void addStuent(Student stud)
+	ArrayList<Student> ar;
+	public boolean addStuent(Student stud)
 	{
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
@@ -20,30 +26,49 @@ public class StudentService {
 			e.printStackTrace();
 		}
 		try {
-			con=DriverManager.getConnection("jdbc:mysql://localhost:3306/jdbctest","root","Sury@171");
+			con=DriverManager.getConnection("jdbc:mysql://localhost:3306/Studentapp","root","Sury@171");
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			System.out.println("kk");
 			e.printStackTrace();
 		}
-//		try {
-//			PreparedStatement pst=con.prepareStatement("insert into studentinfo (name,roll) values(?,?)");
-//			pst.setString(1,stud.getName());
-//			pst.setInt(2, stud.getRoll());
-//			Boolean b=pst.execute();
-//			this.b=b;
-//			
-//			
-//		} catch (SQLException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
-//		return this.b;
+		try {
+			PreparedStatement pst=con.prepareStatement("insert into studentinfo (name,roll) values(?,?)");
+			pst.setString(1,stud.getName());
+			pst.setInt(2, stud.getRoll());
+			Boolean b=pst.execute();
+			this.b=b;
+			
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return this.b;
 		
 	}
-//	public List<Student> getAllStudents()
-//	{
-//		
-//	}
+	public List<Student> getAllStudents()
+	{
+		try {
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		con=DriverManager.getConnection("jdbc:mysql://localhost:3306/Studentapp","root","Sury@171");
+		PreparedStatement pst=con.prepareStatement("select * from studentinfo");
+		ResultSet rs=pst.executeQuery();
+		ar= new ArrayList<Student>();
+		while(rs.next())
+		{
+			Student s=new Student();
+			s.setName(rs.getString(1));
+			s.setRoll(rs.getInt(2));
+			ar.add(s);
+		}
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+		return ar;
+		
+	}
 
 }
